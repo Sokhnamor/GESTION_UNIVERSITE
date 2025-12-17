2109cac82847ea8467f7a410551c2c1ef9bb3e15
Bassirou <bassdr27@gmail.com>
2025-12-17 08:33:11 +0000

    'teste'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerDashboard;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', [ControllerDashboard::class, 'index'])->name('dashboard');
+   
+
+Route::get('/gestionModule', function () {
+    return view('gestionModule')->name('gestionModule');
+});
+
+Route::get('/importation', function () {
+    return view('importation')->name('importation');
 });
