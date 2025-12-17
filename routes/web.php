1a35abdb01a8fa5964b009a9ecaf8884b2758e87
sokhnammor <elhadjimorhana.sokhna@uahb.sn>
2025-12-17 16:16:11 +0000

    modification2

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ExportController;
-use App\Http\Controllers\GradeValidationController;
 use App\Http\Controllers\ControllerImportation;
 use App\Http\Controllers\ControllerDashboard;
+use App\Http\Controllers\ExportController;
 use App\Http\Controllers\ControllerGestionModule;
+use App\Http\Controllers\GradeValidationController;
 
 
 Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::put('/profile', [ProfileController::class, 'updatePassword'])->name('profile.update');
-Route::get('/export', [ExportController::class, 'index'])->name('export');
+route::get('/export', [ExportController::class, 'index'])->name('export');
 Route::get('/grades-validation', [GradeValidationController::class, 'index'])->name('gradevalidation');
    
 
+Route::get('/dashboard', [ControllerDashboard::class, 'mor'])->name('dashboard');
+Route::get('/gestionModule', [ControllerGestionModule::class, 'diop']) ->name('gestionModule'); 
+Route::get('/importation', [ControllerImportation::class, 'bass']) ->name('importation'); 
+
+
+   
 
