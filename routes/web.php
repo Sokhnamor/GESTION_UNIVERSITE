9ccd2343473d31cda1d540ac4b853b26182941ca
sokhnammor <elhadjimorhana.sokhna@uahb.sn>
2025-12-17 15:29:08 +0000

    retouchement route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ExportController;
+use App\Http\Controllers\GradeValidationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::put('/profile', [ProfileController::class, 'updatePassword'])->name('profile.update');
+Route::get('/export', [ExportController::class, 'index'])->name('export');
+Route::get('/grades-validation', [GradeValidationController::class, 'index'])->name('gradevalidation');
+   
+
+
+
+
+
+   
+
